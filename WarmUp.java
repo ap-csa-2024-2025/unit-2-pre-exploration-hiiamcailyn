87484cfa38c13dc87c9a01de976391b60e1964f5
hiiamcailyn <cailyntran@gmail.com>
2024-09-24 09:44:48 -0700

    yay

--- WarmUp.java
+++ WarmUp.java
@@ -2,9 +2,19 @@ public class WarmUp
 {
   public static void main(String[] args)
   {
-    // test your methods here
+    System.out.print("Hello World");
+    boolean foo = isDivisible(25, 5);
+
+  }
+  public static boolean isEven(int n)
+  {
+    return (n%2==0);
   }
 
+  public static boolean isDivisble(int a, int b)
+  {
+    return(x % y ==0);
+  }
   // write your methods below
 
   
